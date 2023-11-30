2bbf421040d4e8d7a02c99d1c9b2e7a4a32b1139
GIYUN9 <giyoon01292@naver.com>
2023-11-30 11:39:19 +0900

    11-30

---
+++ mailProject/src/main/java/com/kh/mail/Test1.java
@@ -0,0 +1,55 @@
+package com.kh.mail;
+
+import java.util.Properties;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+//직접 도구를 생성해서 이메일을 보내보자
+
+//필요한것
+//gmail계정
+
+/*
+ * Email Protocol
+ * SMTP
+ * 이메일을 전송할 때 사용하는 프로토콜(전송방식(규칙))
+ * 
+ * POP
+ * 이메일 서버에 도착한 메일을 클라이언트로 가져올 때 사용하는 프로토콜
+ * 
+ * IMAP
+ * 이메일 서버에 직접 접속하여 이메일을 확인할 때 사용하는 프로토콜
+ */
+public class Test1 {
+	public static void main(String[] args) {
+		
+		// MIME 형식의 메일을 보내기 위해서 JavaMailSender 인터페이스를 사용한다.
+		// 계정설정
+		JavaMailSenderImpl sender = new JavaMailSenderImpl();
+		sender.setHost("smtp.gmail.com");
+		sender.setPort(587); //gmail: 587 다른메일은 찾아봐야한다.
+		sender.setUsername("GIYUN8520@gmail.com");
+		sender.setPassword("brpw~~~"); //깃에 올린때 반드시 지울것.
+		
+		//옵션설정
+		Properties prop = new Properties();
+		prop.put("mail.smtp.auth", true);
+		prop.put("mail.smtp.starttls.enable", true);
+		
+		sender.setJavaMailProperties(prop);
+		
+		//메세지 생성
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setSubject("이메일 전송 테스트(제목)");
+		message.setText("이메일 전송 테스트 첫번째입니다.(내용)");
+		
+		String[] to = {"GIYUN8520@gmail.com"};
+		message.setTo(to);
+		
+		String[] cc = {"GIYUN8520@gmail.com"};
+		message.setCc(cc);
+		
+		sender.send(message);
+	}
+}
---
+++ mailProject/src/main/java/com/kh/mail/Test2.java
@@ -0,0 +1,93 @@
+package com.kh.mail;
+
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@Controller
+public class Test2 {
+	
+	@Autowired
+	private JavaMailSender sender;
+	
+	@RequestMapping(value = "send")
+	public String mail() {
+		//메세지 생성
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setSubject("이메일 전송 테스트(제목)2");
+		message.setText("이메일 전송 테스트 두번째입니다.(내용)2");
+		
+		String[] to = {"GIYUN8520@gmail.com"};
+		message.setTo(to);
+		
+		String[] cc = {"GIYUN8520@gmail.com"};
+		message.setCc(cc);
+		
+		sender.send(message);
+		
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value = "hypermail")
+	public String hypermail() throws MessagingException {
+		//메일을 보내는 표준양식 MIME를 통해서 메일을 전송해보자
+		MimeMessage message = sender.createMimeMessage();
+		
+		MimeMessageHelper helper = new MimeMessageHelper(message, false, "UTF-8");
+		
+		String[] to = {"GIYUN8520@gmail.com"};
+		helper.setTo(to);
+		
+		String[] cc = {"GIYUN8520@gmail.com"};
+		helper.setCc(cc);
+		
+		helper.setSubject("이메일 전송 테스트(제목)3");
+		//두번째 인자를 true로 보낼 시 html 사용하겠다라는 의미
+		String url = ServletUriComponentsBuilder
+				.fromCurrentContextPath()
+				.port(8010).path("/product")
+				.queryParam("product_id", 1002)
+				.toUriString();
+				
+		helper.setText("<a href='"+ url + "'>웹사이트로 이동</a>", true);
+		
+		sender.send(message);
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value = "sendfile")
+	public String sendfile() throws MessagingException {
+		//메일을 보내는 표준양식 MIME를 통해서 메일을 전송해보자
+		MimeMessage message = sender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+		
+		String[] to = {"GIYUN8520@gmail.com"};
+		helper.setTo(to);
+		
+		String[] cc = {"GIYUN8520@gmail.com"};
+		helper.setCc(cc);
+		
+		helper.setSubject("이메일 전송 테스트(제목)4");
+		//두번째 인자를 true로 보낼 시 html 사용하겠다라는 의미
+				
+		
+		helper.setText("파일 전송합니다");
+		
+		//첨부파일 추가
+		// javax.activation.DataSources : 파일정보
+		DataSource source = new FileDataSource("C:\\Users\\user1\\Downloads\\이미지저장용테스트빵빵이.jpg");
+		helper.addAttachment(source.getName(), source);
+		
+		sender.send(message);
+		return "redirect:/";
+	}
+}
